ef5d5dd683f6f88175b3401de3c2bcdcd6c2b154
Radoncode <relbouni69@gmail.com>
2022-01-19 14:38:12 +0000

    adding a picture , change the properties of a svg picture using the props

---
+++ src/Form.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import Car from './Car';
+
+class Form extends Component {
+
+    render(){
+        return (
+            <div>
+                <Car color="red" height="400"  />
+                <h1>Commentaire</h1>
+                Formulaire
+            </div>
+        )
+    }
+}
+
+export default Form;
+
